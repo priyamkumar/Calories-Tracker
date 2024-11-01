0cc1755acc7d44b93282251f05f3c3a19a683696
PRIYAM <you@example.com>
2024-11-01 23:00:51 +0530

    fontawesome icon setup

--- src/Components/AddMeal.jsx
+++ src/Components/AddMeal.jsx
@@ -1,36 +1,46 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faMugSaucer, faBreadSlice, faBowlRice, faAppleWhole, faPlus} from '@fortawesome/free-solid-svg-icons'
 
-export default function AddMeal() {
-  return (
+export default function AddMeal() {  return (
     <div className="meal-container">
       <div className="meal-card">
-        <h3>Breakfast</h3>
-        <p>100 / 640 Cal</p>
-      </div>
-      <div className="add">
-        <button><i class="fa-solid fa-plus"></i></button>
-      </div>
-      <div className="meal-card">
-        <h3>Lunch</h3>
-        <p>100 / 640 Cal</p>
-        <div className="add">
-        <button><i class="fa-solid fa-plus"></i></button>
-      </div>
+        <div>
+        
+          <h3><FontAwesomeIcon icon={faMugSaucer} /> Breakfast</h3>
+          <p>100 / 640 Cal</p>
+        </div>
+        <button>
+        <FontAwesomeIcon icon={faPlus} />
+        </button>
       </div>
       <div className="meal-card">
-        <h3>Dinner</h3>
-        <p>100 / 640 Cal</p>
-        <div className="add">
-        <button><i class="fa-solid fa-plus"></i></button>
-      </div>
+        <div>
+          <h3><FontAwesomeIcon icon={faBreadSlice} /> Lunch</h3>
+          <p>100 / 640 Cal</p>
+        </div>
+        <button>
+        <FontAwesomeIcon icon={faPlus} />
+        </button>
       </div>
       <div className="meal-card">
-        <h3>Snacks</h3>
-        <p>100 / 640 Cal</p>
-        <div className="add">
-        <button><i class="fa-solid fa-plus"></i></button>
+        <div>       
+          <h3><FontAwesomeIcon icon={faBowlRice} /> Dinner</h3>
+          <p>100 / 640 Cal</p>
+        </div>
+        <button>
+        <FontAwesomeIcon icon={faPlus} />
+        </button>
       </div>
+      <div className="meal-card">
+        <div>
+          <h3><FontAwesomeIcon icon={faAppleWhole} /> Snacks</h3>
+          <p>100 / 640 Cal</p>
+        </div>
+        <button>
+        <FontAwesomeIcon icon={faPlus} />
+        </button>
       </div>
     </div>
-  )
+  );
 }
