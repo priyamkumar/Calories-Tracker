b8217261c37d635fc1e2726824539791234a9256
PRIYAM <you@example.com>
2024-11-23 13:20:46 +0530

    added bmr and daily calorie need

--- src/Components/StatisticsAverages.jsx
+++ src/Components/StatisticsAverages.jsx
@@ -27,26 +27,41 @@ export default function StatisticsAverages() {
   let averageWeekFats = Math.round(fats.reduce((acc, cur) => acc + cur, 0) / 7);
 
   let userData = JSON.parse(localStorage.getItem("userData"));
-  console.log(typeof userData.weight);
+  console.log(userData);
+  userData.weight = Number(userData.weight);
+  userData.height = Number(userData.height);
   let bmi = 0;
   if (userData.height && userData.weight)
     bmi = (
       (userData.weight / (userData.height * userData.height)) *
       10000
-    ).toFixed(2);
-  console.log(bmi);
+    ).toFixed(1);
+  let bmr = userData.gender === "Male" ? (10 * userData.weight) + (6.25 * userData.height) - (5 * userData.age) + 5 : (10 * userData.weight) + (6.25 * userData.height) - (5 * userData.age) - 161;
+  bmr = bmr > 0 ? bmr : 0;
+  function multiplierCalculator() {
+    switch (userData.level) {
+      case "sedentary" : return Math.round(bmr * 1.2);
+      case "lightly_active" : return Math.round(bmr * 1.375);
+      case "moderately_active" : return Math.round(bmr * 1.55);
+      case "very_active" : return Math.round(bmr * 1.725);
+      default : return 0;
+    }
+  }
+  let calorieNeed = multiplierCalculator();
 
   return (
     <div className="average-cal">
+      <h3>
+        BMI (Body Mass Index): {bmi} kg/m<sup>2</sup>
+      </h3>
+      <h3>BMR (Basal Metabolic Rate): {bmr} Calories/day</h3>
+      <h3>Daily calorie needs based on activity level: {calorieNeed} Calories</h3>
       <h3>Today's Remaining Calories: {remainingCal > 0 ? remainingCal : 0}</h3>
       <h3>Last 7 days :-</h3>
       <h3>Average Calories: {averageWeekCal} Cal</h3>
       <h3>Average Carbs: {averageWeekCarbs} g</h3>
       <h3>Average Protein: {averageWeekProtein} g</h3>
       <h3>Average Fats: {averageWeekFats} g</h3>
-      <h3>
-        BMI: {bmi} kg/m<sup>2</sup>
-      </h3>
     </div>
   );
 }
